cec217974cf6445fca2da4daffc0c1576c9a5a41
unknown <rmnc@aluno.ifnmg.edu.br>
2023-06-30 22:58:42 -0300

    Alterando html e js

--- script01.js
+++ script01.js
@@ -6,7 +6,7 @@ function verificar() {
   if (fano.value.length == 0 || Number (fano.value) > ano) {
     window.alert('[ERRO] Verifique os dados e tente novamente!')
   } else {
-    var fsex = document.getElementById('radsex')
+    var fsex = document.getElementsByName('radsex')
     var idade = ano - Number (fano.value)
     var gênero = ''
     var img = document.createElement('img')
